fe5a7ffff355038cd608844def9623978dc32530
michmanaa <mytych-2004@wp.pl>
2025-03-11 00:02:50 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import TasksLab01.RunTasks;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        // obiekt klasy Run
+        RunTasks runTasks = new RunTasks();
+        //wywoałnie metody Run
+        runTasks.Run();
     }
 }
